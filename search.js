6266e293821d03f3441e9ea21fb0d36de492e23d
Dylan <heyitsdylan@gmail.com>
2021-08-29 22:06:51 -0700

    binary search

--- search.js
+++ search.js
@@ -6,3 +6,25 @@ const linearSearch = (id, arr) => {
   }
   return void 0;
 }
+
+const binarySearch = (id, arr) => {
+  let min = 0;
+  let max = arr.length - 1;
+  let index;
+  let element;
+
+  while (min <= max) {
+    index = Math.floor((min + max) / 2);
+    element = arr[index];
+
+    if (element.id > id) {
+      max = index - 1;
+    } else if (element.id < id) {
+      min = index + 1;
+    } else {
+      return element;
+    }
+  }
+
+  return void 0;
+}
