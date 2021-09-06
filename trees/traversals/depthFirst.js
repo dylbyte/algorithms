43a8a3937ccbfd92aeefcb092f28aa15dad52a06
Dylan <heyitsdylan@gmail.com>
2021-09-05 20:50:51 -0700

    outline methods

--- trees/traversals/depthFirst.js
+++ trees/traversals/depthFirst.js
@@ -1,11 +1,17 @@
+// useful to deep copy a tree
 const preorderTraverse = (node, array) => {
-  // code goes here
+  // starting at the root, visit all the nodes
+  // add to the array, go left first to the bottom, then right
+  // go as deep as you can before coming back up
 };
 
+// useful to return sorted list from tree
 const inorderTraverse = (node, array) => {
-  // code goes here
+  // go left, add to the array, go right
 };
 
+// useful in delete operations
 const postorderTraverse = (node, array) => {
-  // code goes here
+  // root always last
+  // go left, go right, add to the array (after visiting the last child)
 };
