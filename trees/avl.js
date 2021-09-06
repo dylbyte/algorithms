f945d8190e326e16ba73f8b08a34c281c59a3128
Dylan <heyitsdylan@gmail.com>
2021-09-05 20:16:14 -0700

    complete implementation

--- trees/avl.js
+++ trees/avl.js
@@ -2,7 +2,6 @@ class AVLTree {
   constructor() {
     this.root = null;
   }
-
   add(value) {
     if (!this.root) {
       this.root = new Node(value);
@@ -22,34 +21,90 @@ class Node {
     this.value = value;
     this.height = 1;
   }
-
   add(value) {
-    let current = this.root;
-    // decide to go left or right
+    if (value < this.value) {
+      if (this.left) {
+        this.left.add(value);
+      } else {
+        this.left = new Node(value);
+      }
+      if (!this.right || this.right.height < this.left.height) {
+        this.height = this.left.height + 1;
+      }
+    } else {
+      if (this.right) {
+        this.right.add(value);
+      } else {
+        this.right = new Node(value);
+      }
+      if (!this.left || this.right.height > this.left.height) {
+        this.height = this.right.height + 1;
+      }
+    }
 
-    // find the correct place to add
-    // make sure to update height
     this.balance();
   }
   balance() {
-    // ask if this node is out of balance -- check height 2 or more
-      // if not out of balance, do nothing
-      // if out of balance, need single or double?
-        // if single -- call rotate on self
-        // if double -- call rotate on child then self
+    const leftHeight = this.left ? this.left.height : 0;
+    const rightHeight = this.right ? this.right.height : 0;
+
+    if (leftHeight > rightHeight + 1) {
+      const leftRightHeight = this.left.right ? this.left.right.height : 0;
+      const leftLeftHeight = this.left.left ? this.left.left.height : 0;
+
+      // double rotation
+      if (leftRightHeight > leftLeftHeight) {
+        this.left.rotateRR();
+      }
+
+      this.rotateLL();
+    } else if (rightHeight > leftHeight + 1) {
+      const rightRightHeight = this.right.right ? this.right.right.height : 0;
+      const rightLeftHeight = this.right.left ? this.right.left.height : 0;
+
+      // double rotation
+      if (rightLeftHeight > rightRightHeight) {
+        this.right.rotateLL();
+      }
+
+      this.rotateRR();
+    }
   }
   rotateRR() {
-    // if right child is heavy
-    // rotate
-    this.right.updateInNewLocation();
+    const valueBefore = this.value;
+    const leftBefore = this.left;
+    this.value = this.right.value;
+    this.left = this.right;
+    this.right = this.right.right;
+    this.left.right = this.left.left;
+    this.left.left = leftBefore;
+    this.left.value = valueBefore;
+    this.left.updateInNewLocation();
     this.updateInNewLocation();
   }
   rotateLL() {
-    // if left child is heavy
-    this.left.updateInNewLocation();
+    const valueBefore = this.value;
+    const rightBefore = this.right;
+    this.value = this.left.value;
+    this.right = this.left;
+    this.left = this.left.left;
+    this.right.left = this.right.right;
+    this.right.right = rightBefore;
+    this.right.value = valueBefore;
+    this.right.updateInNewLocation();
     this.updateInNewLocation();
   }
   updateInNewLocation() {
     // calculate new height
+    if (!this.right && !this.left) {
+      this.height = 1;
+    } else if (
+      !this.right || 
+      (this.left && this.right.height < this.left.height)
+      ) {
+        this.height = this.left.height + 1;
+      } else {
+        this.height = this.right.height + 1;
+      }
   }
 }
