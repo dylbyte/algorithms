0cbb8c1d251c7f5db4edd7199bddfb41a8990c05
Dylan <heyitsdylan@gmail.com>
2021-09-05 20:36:31 -0700

    init depth first traversal

---
+++ trees/traversals/depthFirst.js
@@ -0,0 +1,11 @@
+const preorderTraverse = (node, array) => {
+  // code goes here
+};
+
+const inorderTraverse = (node, array) => {
+  // code goes here
+};
+
+const postorderTraverse = (node, array) => {
+  // code goes here
+};
