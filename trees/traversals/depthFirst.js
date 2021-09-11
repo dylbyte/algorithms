1d3c36ee286fdaa7b17557546e623cc7ea1f32ec
Dylan <heyitsdylan@gmail.com>
2021-09-10 22:09:09 -0700

    implement inorder

--- trees/traversals/depthFirst.js
+++ trees/traversals/depthFirst.js
@@ -3,11 +3,21 @@ const preorderTraverse = (node, array) => {
   // starting at the root, visit all the nodes
   // add to the array, go left first to the bottom, then right
   // go as deep as you can before coming back up
+  if (!node) return array;
+  array.push(node.value);
+  array = preorderTraverse(node.left, array);
+  array = preorderTraverse(node.right, array);
+  return array;
 };
 
 // useful to return sorted list from tree
 const inorderTraverse = (node, array) => {
   // go left, add to the array, go right
+  if (!node) return array;
+  array = inorderTraverse(node.left, array);
+  array.push(node.value);
+  array = inorderTraverse(node.right, array);
+  return array;
 };
 
 // useful in delete operations
