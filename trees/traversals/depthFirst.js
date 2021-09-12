b33317dd2607a8a7d90bff8965ff49fac5fbd997
Dylan <heyitsdylan@gmail.com>
2021-09-11 23:55:08 -0700

    add node class

--- trees/traversals/depthFirst.js
+++ trees/traversals/depthFirst.js
@@ -1,8 +1,5 @@
 // useful to deep copy a tree
-const preorderTraverse = (node, array) => {
-  // starting at the root, visit all the nodes
-  // add to the array, go left first to the bottom, then right
-  // go as deep as you can before coming back up
+const preorderTraverse = (node, array = []) => {
   if (!node) return array;
   array.push(node.value);
   array = preorderTraverse(node.left, array);
@@ -11,8 +8,7 @@ const preorderTraverse = (node, array) => {
 };
 
 // useful to return sorted list from tree
-const inorderTraverse = (node, array) => {
-  // go left, add to the array, go right
+const inorderTraverse = (node, array = []) => {
   if (!node) return array;
   array = inorderTraverse(node.left, array);
   array.push(node.value);
@@ -21,12 +17,18 @@ const inorderTraverse = (node, array) => {
 };
 
 // useful in delete operations
-const postorderTraverse = (node, array) => {
-  // root always last
-  // go left, go right, add to the array (after visiting the last child)
+const postorderTraverse = (node, array = []) => {
   if (!node) return array;
   array = postorderTraverse(node.left, array);
   array = postorderTraverse(node.right, array);
   array.push(node.value);
   return array;
 };
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
