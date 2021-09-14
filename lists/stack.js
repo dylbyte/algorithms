b0ea5fbba2651aae9191ef35c46fa3ff882903a2
Dylan <heyitsdylan@gmail.com>
2021-09-14 14:12:08 -0700

    implement stack class

---
+++ lists/stack.js
@@ -0,0 +1,32 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(item) {
+    this.items.push(item);
+  }
+
+  pop() {
+    const top = this.peek();
+    this.items.pop();
+    return top;
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  clear() {
+    this.items = [];
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+}
