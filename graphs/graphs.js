3a78bd9e7dcb7e6e504aac3792026c7a8232f918
Dylan <heyitsdylan@gmail.com>
2021-10-04 22:27:24 -0700

    clean up

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -40,14 +40,5 @@ const findMostCommonTitle = (myId, degreesOfSeparation) => {
     }
   }
 
-  // see all job titles, sorted
-  // jobKeys
-  //   .map((id) => [id, jobs[id]])
-  //   .sort((a, b) => b[1] - a[1])
-  //   .slice(0, 10)
-  //   .forEach(([job, num]) => console.log(`${num} – ${job}`));
-
-  // console.log("======");
-
   return jobName;
 };
