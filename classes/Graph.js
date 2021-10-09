223c11ecbf4f2db884bd9e4a52f0e046ea1e08bb
Dylan <heyitsdylan@gmail.com>
2021-10-09 00:11:19 -0700

    adds additional comments

--- classes/Graph.js
+++ classes/Graph.js
@@ -9,9 +9,10 @@ class Graph {
     adjacents.set(v, []); // initialize the adjacent list for given vtx
   }
 
+  // adds an edge between two vertices (v, w)
   #addEdge(v, w) {
-    adjacents.get(v).push(w);
-    adjacents.get(w).push(v);
+    adjacents.get(v).push(w); // add w to vertex v's adjacents list
+    adjacents.get(w).push(v); // add v to the adjacents list of vertex w
   }
 }
 
