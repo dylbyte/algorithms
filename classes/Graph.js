6c2682ec739cbd006cd3384acb9cb10015cbe8e5
Dylan <heyitsdylan@gmail.com>
2021-10-08 23:59:03 -0700

    implement Graph class methods

--- classes/Graph.js
+++ classes/Graph.js
@@ -3,6 +3,10 @@ class Graph {
     this.#vertices = [];
     this.#adjacents = {};
   }
+  addVertex(v) {
+    vertices.push(v); // adds vertex to list of vertices
+    adjacents.set(v, []); // initialize the adjacent list for given vtx
+  }
 }
 
 module.exports = Graph;
