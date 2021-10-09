970c687d994e074a8d94bb4651bad38b05598318
Dylan <heyitsdylan@gmail.com>
2021-10-08 23:59:48 -0700

    continue Graph private methods

--- classes/Graph.js
+++ classes/Graph.js
@@ -3,9 +3,15 @@ class Graph {
     this.#vertices = [];
     this.#adjacents = {};
   }
-  addVertex(v) {
+
+  #addVertex(v) {
     vertices.push(v); // adds vertex to list of vertices
     adjacents.set(v, []); // initialize the adjacent list for given vtx
+  }
+
+  #addEdge(v, w) {
+    adjacents.get(v).push(w);
+    adjacents.get(w).push(v);
   }
 }
 
