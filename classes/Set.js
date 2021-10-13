502f9d890f54f3b3037cce18d5a2702c54540111
Dylan <heyitsdylan@gmail.com>
2021-10-12 21:51:31 -0700

    init Set class

---
+++ classes/Set.js
@@ -0,0 +1,30 @@
+class Set {
+  constructor() {
+    this.items = {};
+  }
+
+  add(value) {
+    // add value to items
+  }
+
+  delete(value) {
+    // remove value from items
+  }
+
+  has(value) {
+    // lookup value in items
+  }
+
+  clear() {
+    // empty the set
+  }
+
+  size() {
+    // return number of items
+  }
+
+  values() {
+    // print the items
+  }
+
+}
